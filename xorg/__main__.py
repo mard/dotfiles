1bf7597adc495323037265faea09c4fa26ee256b
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-05-13 18:46:34 +0200

    add xorg

---
+++ xorg/__main__.py
@@ -0,0 +1,12 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('xorg-server')
+PackageInstaller.try_install('xorg-xinit')
+PackageInstaller.try_install('xorg-xrandr')
+PackageInstaller.try_install('xdotool')
+PackageInstaller.try_install('pkg-config')
+
+FileInstaller.create_symlink(os.path.join(dir, 'xinitrc'), '~/.xinitrc')
