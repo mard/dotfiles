0dcfe241e491a5dd1a42cc630843adf106984017
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-05-28 19:29:16 +0200

    change tmux to use external statusbar script

--- tmux/__main__.py
+++ tmux/__main__.py
@@ -5,3 +5,4 @@ dir = os.path.dirname(__file__)
 
 PackageInstaller.try_install('tmux')
 FileInstaller.create_symlink(os.path.join(dir, 'tmux.conf'), '~/.tmux.conf')
+FileInstaller.create_symlink(os.path.join(dir, 'tmux.statusbar'), '~/.tmux.statusbar')
