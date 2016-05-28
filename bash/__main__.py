c872e07b9b9a3c2a6885bd928b4be3c15611df90
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-05-29 00:51:07 +0200

    add bash

---
+++ bash/__main__.py
@@ -0,0 +1,8 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('bash')
+FileInstaller.create_symlink(os.path.join(dir, 'bash_profile'), '~/.bash.profile')
+FileInstaller.create_symlink(os.path.join(dir, 'bashrc'), '~/.bashrc')
