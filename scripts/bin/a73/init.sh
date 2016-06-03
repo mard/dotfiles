7ccab3be0f0157a8e332819d299332a4094f2e54
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-06-03 17:20:01 +0200

    change A73 frequency governor

--- scripts/bin/a73/init.sh
+++ scripts/bin/a73/init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# set CPU frequency configuration
-echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+# set CPU frequency governor configuration
+echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
 # enable CPU freq reading
 chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
