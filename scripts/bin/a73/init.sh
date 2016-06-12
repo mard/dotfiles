0d0ae164230058bdd4f5fa2720ed9d99d42e642e
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-06-12 20:11:56 +0200

    change a73 init script

--- scripts/bin/a73/init.sh
+++ scripts/bin/a73/init.sh
@@ -8,3 +8,5 @@ chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
 # enable suspend
 chmod 666 /sys/power/state
 
+# expose brightness control
+chmod 666 /sys/class/pwm-sunxi/pwm0/duty_percent
