82472f648139fcb4273b049eefe1670d20beb884
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-07-20 15:55:47 +0200

    change a73 suspend script

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -3,6 +3,7 @@ import os
 from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
+PackageInstaller.try_install('slock')
 
 FileInstaller.create_symlink(os.path.join(dir, 'bin'), '~/scripts/bin')
 FileInstaller.create_symlink(os.path.join(dir, 'lib'), '~/scripts/lib')
