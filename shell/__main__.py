ce0304a69c17b7c35475ac9ee99aede2e58de94b
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-12-11 13:05:39 +0100

    fix shell missing symlink

--- shell/__main__.py
+++ shell/__main__.py
@@ -7,4 +7,4 @@ PackageInstaller.try_install('bash')
 FileInstaller.create_symlink(os.path.join(dir, 'bash_profile'), '~/.bash_profile')
 FileInstaller.create_symlink(os.path.join(dir, 'bashrc'), '~/.bashrc')
 FileInstaller.create_symlink(os.path.join(dir, 'inputrc'), '~/.inputrc')
-FileInstaller.create_symlink(os.path.join(dir, 'locale.conf'), '~/.config/locale.conf')
+
