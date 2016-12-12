8ffb793a8cb1485e269c7a856687f84c638e1b4b
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-12-12 19:07:02 +0100

    fix xorg run command

--- xorg/__main__.py
+++ xorg/__main__.py
@@ -1,13 +1,7 @@
 #!/bin/python
 import os
-from libinstall import FileInstaller, PackageInstaller
+from libinstall import FileInstaller, PackageInstaller, run_verbose
 dir = os.path.dirname(__file__)
-
-PackageInstaller.try_install('xorg-server')
-PackageInstaller.try_install('xorg-xinit')
-PackageInstaller.try_install('xorg-xrandr')
-PackageInstaller.try_install('xdotool')
-PackageInstaller.try_install('pkg-config')
 
 FileInstaller.create_symlink(os.path.join(dir, 'xinitrc'), '~/.xinitrc')
 FileInstaller.create_symlink(os.path.join(dir, 'Xresources'), '~/.config/Xresources')
