63094c2dbe158970c969a6f447075b743af24e29
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2017-02-09 20:21:27 +0100

    add mpv

---
+++ mpv/__main__.py
@@ -0,0 +1,8 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('mpv')
+FileInstaller.create_symlink(os.path.join(dir, 'mpv.conf'), '~/.config/mpv/mpv.conf')
+
