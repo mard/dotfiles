a66dafc18249c491a7eacd9e662324b9bae22212
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2017-02-15 19:43:12 +0100

    add mpv input configuration

--- mpv/__main__.py
+++ mpv/__main__.py
@@ -5,4 +5,5 @@ dir = os.path.dirname(__file__)
 
 PackageInstaller.try_install('mpv')
 FileInstaller.create_symlink(os.path.join(dir, 'mpv.conf'), '~/.config/mpv/mpv.conf')
+FileInstaller.create_symlink(os.path.join(dir, 'input.conf'), '~/.config/mpv/input.conf')
 
