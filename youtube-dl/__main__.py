944af1c6dd8c316d4961ffdb8b6737ea7f428028
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2018-09-09 15:36:44 +0200

    add youtube-dl

---
+++ youtube-dl/__main__.py
@@ -0,0 +1,8 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('youtube-dl')
+
+FileInstaller.create_symlink(os.path.join(dir, 'config'), '~/.config/youtube-dl/config')
