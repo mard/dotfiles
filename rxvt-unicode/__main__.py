068a26bdf9ba1ba40275b133a74486cb3f63fc35
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2018-09-16 17:41:31 +0200

    fix xorg Xresources management

--- rxvt-unicode/__main__.py
+++ rxvt-unicode/__main__.py
@@ -7,4 +7,4 @@ PackageInstaller.try_install('rxvt-unicode')
 PackageInstaller.try_install('xorg-fonts-misc')
 
 FileInstaller.create_symlink(os.path.join(dir, 'rxvt-unicode'), '~/.Xresources.d/rxvt-unicode')
-run_verbose(['xrdb', '-I$HOME ' + os.path.expanduser('~/.Xresources')])
+run_verbose(['xrdb', '-merge', os.path.expanduser('~/.Xresources.d/rxvt-unicode')])
