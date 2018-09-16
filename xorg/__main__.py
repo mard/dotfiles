d856c55be91252e7d7bef1b417299ca18013a8b6
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2018-09-16 14:23:32 +0200

    fix xorg Xresources path

--- xorg/__main__.py
+++ xorg/__main__.py
@@ -4,7 +4,7 @@ from libinstall import FileInstaller, PackageInstaller, run_verbose
 dir = os.path.dirname(__file__)
 
 FileInstaller.create_symlink(os.path.join(dir, 'xinitrc'), '~/.xinitrc')
-FileInstaller.create_symlink(os.path.join(dir, 'Xresources'), '~/.config/Xresources')
+FileInstaller.create_symlink(os.path.join(dir, 'Xresources'), '~/.Xresources')
 FileInstaller.create_symlink(os.path.join(dir, 'imwheelrc'), '~/.imwheelrc')
 run_verbose(['xrdb', os.path.expanduser('~/.config/Xresources')])
 
