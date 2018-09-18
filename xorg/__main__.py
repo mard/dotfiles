9793164fe46b734b59292b4b2b500e78b2e74a95
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2018-09-18 20:25:30 +0200

    add xorg packages

--- xorg/__main__.py
+++ xorg/__main__.py
@@ -3,6 +3,11 @@ import os
 from libinstall import FileInstaller, PackageInstaller, run_verbose
 dir = os.path.dirname(__file__)
 
+PackageInstaller.try_install('xorg-server')
+PackageInstaller.try_install('xorg-xinit')
+PackageInstaller.try_install('xorg-xset')
+PackageInstaller.try_install('xorg-xrdb')
+
 FileInstaller.create_symlink(os.path.join(dir, 'xinitrc'), '~/.xinitrc')
 FileInstaller.create_symlink(os.path.join(dir, 'Xresources'), '~/.Xresources')
 FileInstaller.create_symlink(os.path.join(dir, 'imwheelrc'), '~/.imwheelrc')
