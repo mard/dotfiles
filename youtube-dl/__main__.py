04229ebb6bb1949f7e0164e20979539bedee01e7
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2018-09-20 22:02:42 +0200

    fix youtube-dl install via pip

--- youtube-dl/__main__.py
+++ youtube-dl/__main__.py
@@ -3,6 +3,6 @@ import os
 from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
-PackageInstaller.try_install('youtube-dl')
+PackageInstaller.try_install('youtube-dl', 'pip')
 
 FileInstaller.create_symlink(os.path.join(dir, 'config'), '~/.config/youtube-dl/config')
