f65d04defdf1581ac703cbbddd9f71d24e9d57d6
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2019-01-30 01:07:21 +0100

    change vscode package

--- vscode/__main__.py
+++ vscode/__main__.py
@@ -3,6 +3,7 @@ import os
 from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
-PackageInstaller.try_install('visual-studio-code-bin')
+PackageInstaller.try_install('code')
 
 FileInstaller.create_symlink(os.path.join(dir, 'settings.json'), '~/.config/Code/User/settings.json')
+FileInstaller.create_symlink(os.path.join(dir, 'settings.json'), '~/.config/Code - OSS/User/settings.json')
