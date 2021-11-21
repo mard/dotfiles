7f1f2c6d9b1d6525f6d4f5f7c6b03c555cce1845
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2021-11-21 16:40:21 +0100

    change youtube-dl to yt-dlp

--- youtube-dl/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-import os
-from libinstall import FileInstaller, PackageInstaller
-dir = os.path.dirname(__file__)
-
-PackageInstaller.try_install('youtube-dl', 'pip')
-
-FileInstaller.create_symlink(os.path.join(dir, 'config'), '~/.config/youtube-dl/config')
